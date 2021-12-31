f7b5c2240ee9cfa1a2cc4f55dd25870af62f7a8d
Franner Ventura <franner.ventura@gmail.com>
2021-12-31 11:56:03 -0300

    [id-02] prueba de creacion de token en staging
    [id-02] prueba de creacion de token en staging

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -20,15 +20,16 @@ router.get('/verifyToken', auth, function(req, res, next) {
 
 router.get('/createToken', function(req, res, next) {
     console.log(`Entering ${req.originalUrl}`);
-    const token = jwt.sign({ foo: 'bar' },
+/*     const token = jwt.sign({ foo: 'bar' },
         process.env.TOKEN_KEY, {
             expiresIn: "2h",
         }
-    );
+    ); 
     console.log("TOKEN: " + token);
     data = {token: token,
     message: `Successful execution of ${req.originalUrl}`}
-    res.status(StatusCodes.OK).json(data);
+    res.status(StatusCodes.OK).json(data);*/
+    res.status(StatusCodes.OK).json("data")
     console.log(`Successful execution of execution of ${req.originalUrl}`);
     console.log(`Finalizing ${req.originalUrl}`);
     return;
