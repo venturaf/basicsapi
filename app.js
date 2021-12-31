4b24dfeb43f6cf5d284f3ebca2249cb889586bcf
Franner Ventura <franner.ventura@gmail.com>
2021-12-31 11:23:25 -0300

    [id-02] se crea modulo de autenticación con token
    [id-02] se crea modulo de autenticación con token

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config(); // VARIABLES DE ENTORNO
+
+var indexRouter = require('./routes/indexRouter');
+var usersRouter = require('./routes/usersRouter');
+var authRouter = require('./routes/authRouter');
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
